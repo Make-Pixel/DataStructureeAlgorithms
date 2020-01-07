d294c84a3d4408dd945aac9ed97b03d7244525f0
NTR牛逼 <58197005+Make-Pixel@users.noreply.github.com>
2020-01-07 10:54:34 +0800

    Add files via upload

---
+++ 人工智能/人工智能/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Net.WebRequestMethods;
+
+namespace Enumera
+{
+    public  class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var Kestring = "人工智能";
+            TestReadingFile(Kestring);
+            Console.WriteLine("aa,bb,cc,dd,ee,ff,gg,hh");
+            TestStreamReaderEnumerable(Kestring);
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        private static void TestStreamReaderEnumerable(string kestring)
+        {
+         
+        }
+
+        private static void TestReadingFile(string kestring)
+        {
+            Console.WriteLine("数量");
+          
+        }
+
+       
+    }
+}
